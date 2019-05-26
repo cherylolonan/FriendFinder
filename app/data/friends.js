8887f482d832ac9a092e0c11e58ee57697e4a05f
shrill <cheryl.olonan@gmail.com>
2019-05-25 20:10:39 -0700

    Commit draft of files

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [
+	
+	{
+		name: "Thor Odinson",
+		photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwir1r7AmbjiAhUENH0KHT-vDuQQjRx6BAgBEAU&url=https%3A%2F%2Fmedium.com%2Fthe-establishment%2Fis-thor-ragnarok-a-subversive-takedown-of-white-supremacy-aec729cf0f40&psig=AOvVaw3oIUKbe2fMCS9EfnrM3AeY&ust=1558925839869644",
+		scores: [
+			"5",
+			"1",
+			"4",
+			"3",
+			"1",
+			"5",
+			"1",
+			"4",
+			"3",
+			"1"
+		]
+	},
+	{
+		name: "Steve Rogers",
+		photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiY9Jf9mbjiAhVjJzQIHWTRCagQjRx6BAgBEAU&url=https%3A%2F%2Fwww.thewrap.com%2Fchris-evans-captain-america-avengers-4%2F&psig=AOvVaw0BBIiylxGk6e83JgVbuIOg&ust=1558925947777549",
+		scores: [
+			"3",
+			"1",
+			"5",
+			"4",
+			"1",
+			"5",
+			"1",
+			"1",
+			"5",
+			"3"
+		]
+	},
+	{
+		name: "Arthur Curry",
+		photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjHi6CuibjiAhXSPH0KHXWrCBQQjRx6BAgBEAU&url=https%3A%2F%2Fwww.empireonline.com%2Fmovies%2Faquaman%2Freview%2F&psig=AOvVaw0Zqc0a_Xjpk0iGVQB1NgnE&ust=1558921454820625",
+		score: [
+			"3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"5",
+			"4"		
+        ]
+    },
+    {
+		name: "Diana Prince",
+		photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwi5gb20i7jiAhVVGDQIHbeyBR4QjRx6BAgBEAU&url=https%3A%2F%2Fnurdpedia.fandom.com%2Fwiki%2FWonder_Woman&psig=AOvVaw33IKNLGcdmiLBCcwVrZV15&ust=1558922053121245",
+		score: [
+			"3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"5",
+			"4"		
+        ]
+    },
+    {
+		name: "Carol Danvers",
+		photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj3pLmjmrjiAhXyHzQIHel4BdUQjRx6BAgBEAU&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fgeektyrant.com%252Fnews%252Fcaptain-marvel-is-said-to-be-the-next-face-and-leader-of-the-mcu-as-we-move-into-phase-4%26psig%3DAOvVaw0zfAMTeIlLhFq_9RlQcZON%26ust%3D1558926048495827&psig=AOvVaw0zfAMTeIlLhFq_9RlQcZON&ust=1558926048495827",
+		score: [
+			"3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"5",
+			"4"		
+        ]
+    },
+    {
+        name: "Natasha Romanoff",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjThpH7mrjiAhUIJDQIHVMZBZwQjRx6BAgBEAU&url=https%3A%2F%2Fcomicvine.gamespot.com%2Fforums%2Fgen-discussion-1%2Fwould-mcu-natasha-romanoff-and-cw-oliver-queen-mak-1949520%2F&psig=AOvVaw1wOQB1hjrXOtSdrbKq0AF_&ust=1558926238778085",
+        scores: [
+            "3",
+            "1",
+            "5",
+            "4",
+            "1",
+            "5",
+            "1",
+            "1",
+            "5",
+            "3"
+        ]
+    }
+];
+
+module.exports = friends;
